8e3760f51f1288b420b8f5338b4010219ed86daa
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-11-28 17:41:50 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev
-             {:dependencies [[hickory "0.7.0"]]}})
+             {:dependencies [[hickory "0.6.0"]]}})
