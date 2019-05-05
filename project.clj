c785f78f184567019a6dc60fc1e06d339bede894
Dominic Freeston <dominic.freeston@gmail.com>
2019-05-05 12:13:36 +0100

    Update to Clojure 1.10 and update tests

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/yogthos/clj-rss"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles {:dev
              {:dependencies [[hickory "0.6.0"]]}})
