49b74edd838a5bf6cba55b6635fd2d9208693d86
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-01-18 14:55:17 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-rss "0.2.5"
+(defproject clj-rss "0.2.6"
   :description "A library for generating RSS feeds from Clojure."
   :url "https://github.com/yogthos/clj-rss"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.xml "0.2.0-alpha6"]]
   :profiles {:dev
-             {:dependencies [[hickory "0.6.0"]]}})
+             {:dependencies [[hickory "0.7.1"]]}})
