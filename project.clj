1c2f0e6fb4156f0a07b5a4b93ec3deee139871d5
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-05-11 12:55:11 -0400

    bumped up version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-rss "0.4.0-SNAPSHOT"
+(defproject clj-rss "0.4.0"
   :description "A library for generating RSS feeds from Clojure."
   :url "https://github.com/yogthos/clj-rss"
   :license {:name "Eclipse Public License"
